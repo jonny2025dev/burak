fd644ea6e538f9bc87e223486d15c8dd309c8b62
jonny2025dev <akhrorbek0704@icloud.com>
2025-06-23 23:14:07 +0900

    H TASK train.ts

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,27 @@
+// TASK H: 
+
+// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
+//  faqat positive qiymatlarni olib string holatda return qilsin
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+function getPositive(arr: number[]): string {
+  let result = "";
+
+  for (let num of arr) {
+    if (num > 0) {
+      result += num.toString();
+    }
+  }
+
+  return result;
+}
+
+console.log(getPositive([1, -4, 3]));
+
+
+
+
+
 //TASK G:
 
 // Yagona parametrga ega function tuzing.
@@ -8,20 +32,20 @@
 // Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
 
 
-function getHighestIndex(arr: number[]): number {
-  let max = arr[0];
-  let index = 0;
+// function getHighestIndex(arr: number[]): number {
+//   let max = arr[0];
+//   let index = 0;
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-      index = i;
-    }
-  }
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//       index = i;
+//     }
+//   }
 
-  return index;
-}
+//   return index;
+// }
 
-console.log(getHighestIndex([5, 21, 12, 21, 8]));
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
 
   
