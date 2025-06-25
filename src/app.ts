3e956e94b222bd26e9a71c7c13eada74ef716075
jonny2025dev <akhrorbek0704@icloud.com>
2025-06-26 01:13:13 +0900

    feat: develop member schema model

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
 app.use('/admin', routerAdmin);      //EJS
-app.use('/', router);           //REACT
+app.use('/', router);                //REACT
 
 
 export default app;  
--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { T }  from "../libs/types/common";
+import MemberService from "../libs/models/Member.service";
 
 const restaurantConroller: T = {}; 
 restaurantConroller.goHome = (req: Request, res: Response) => {
