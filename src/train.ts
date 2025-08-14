5680975a3cc745696971779e8a6ea17ee9eadd0a
jonny2025dev <akhrorbek0704@icloud.com>
2025-08-14 18:07:29 +0900

    feat : TASK Z

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,37 @@
+//  ZTASK Shunday function yozing.
+//  Bu function sonlardan iborat array qabul qilsin. 
+//  Function'ning vazifasi array tarkibidagi juft sonlarni topib ularni yig'disini qaytarsin.
+//   MASALAN: sumEvens([1, 2, 3]); return 2; sumEvens([1, 2, 3, 2]); return 4;
+//    Yuqoridagi misolda, bizning funktsiya berilayotgan array tarkibidagi sonlar ichidan faqatgina 
+//    juft bo'lgan sonlarni topib, ularni hisoblab yig'indisini qaytarmoqda.
+
+
+function sumEvens(arr: number[]): number {
+  let sum = 0;
+  for (const num of arr) {
+    if (num % 2 === 0) {
+      sum += num;
+    }
+  }
+  return sum;
+}
+console.log(sumEvens([1, 2, 3]));       
+console.log(sumEvens([1, 2, 3, 2]));    
+
+
+
 // TASK Y  Shunday function yozing, uni 2'ta array parametri bo'lsin. 
 // Bu function ikkala arrayda ham ishtirok etgan bir xil qiymatlarni yagona arrayga joylab qaytarsin.
 //   MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]  Yuqoridagi misolda, 
 //   argument sifatida berilayotgan array'larda o'xshash sonlar mavjud.
 //    Function'ning vazifasi esa ana shu ikkala array'da ishtirok etgan o'xshash sonlarni yagona arrayga joylab return qilmoqda.
 
-function findIntersection(arr1: number[], arr2: number[]): number[] {
-  return arr1.filter((value) => arr2.includes(value));
-}
+// function findIntersection(arr1: number[], arr2: number[]): number[] {
+//   return arr1.filter((value) => arr2.includes(value));
+// }
 
-const result = findIntersection([1, 2, 3], [3, 2, 0]);
-console.log(result); 
+// const result = findIntersection([1, 2, 3], [3, 2, 0]);
+// console.log(result); 
 
 
 // TASK X
